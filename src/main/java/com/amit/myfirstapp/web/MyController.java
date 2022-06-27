af2b2bddb87c2400e61e15468f0ea5f29da7b4ad
Bhawana Gaur <bhawana.gaur@srijan.net>
2022-06-27 10:51:21 +0530

    First commit

---
+++ src/main/java/com/amit/myfirstapp/web/MyController.java
@@ -0,0 +1,22 @@
+package com.amit.myfirstapp.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MyController {
+
+	@GetMapping("/hello/{name}")
+	public String sayHello(@PathVariable String name) {
+		return "Hello " +name;
+	}
+	
+	@GetMapping("/greeting/{name}")
+	public String greeting(@PathVariable String name) {
+		return "Good Morning " +name;
+	}
+	
+}
